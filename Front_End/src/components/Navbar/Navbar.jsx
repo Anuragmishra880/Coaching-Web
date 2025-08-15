7177c2dd81fa9a437a88a484dcbdf60fa1bfb491
Anuragmishra880 <anuragmishra9821497411@gmail.com>
2025-08-15 18:03:30 +0530

    fix reload issue when home link is clicked

--- Front_End/src/components/Navbar/Navbar.jsx
+++ Front_End/src/components/Navbar/Navbar.jsx
@@ -15,6 +15,31 @@ const Navbar = () => {
         })
     }, [])
 
+
+    const handleScroll = (e, targetId) => {
+        e.preventDefault();
+        const target = document.getElementById(targetId);
+        if (!target) return;
+
+        const offset = 70; // Navbar height
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = target.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - offset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+
+        // Mobile menu close
+        const menu = document.querySelector('.navbar-collapse');
+        if (menu && menu.classList.contains('show')) {
+            menu.classList.remove('show');
+        }
+    };
+
+
     return (
         <>
             <nav className={`navbar navbar-expand-lg  navbar-dark  fixed-top shadow-sm  ${sticky ? 'nav-dark' : ''} `}>
@@ -25,11 +50,11 @@ const Navbar = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto align-items-center gap-2">
-                            <li className="nav-item"><a className="nav-link active px-3 py-2" href="home">Home</a></li>
-                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#program">Program</a></li>
-                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#about">About</a></li>
-                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#campus">Campus</a></li>
-                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#contact">Contact</a></li>
+                            <li className="nav-item"><a className="nav-link active px-3 py-2" href="home" onClick={(e, "home")}>Home</a></li>
+                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#program" onClick={(e, "program")}>Program</a></li>
+                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#about" onClick={(e, "about")}>About</a></li>
+                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#campus" onClick={(e, "campus")}>Campus</a></li>
+                            <li className="nav-item"><a className="nav-link px-3 py-2" href="#contact" onClick={(e, "contact")}>Contact</a></li>
 
                         </ul>
                     </div>
